cafafc17ae15fa49ae66f9e0bef91f832e90708d
MadihaFarman <madihafarman22280@gmail.com>
2026-06-04 16:22:13 +0500

    output Parsers : PydnaticOutputParser

---
+++ 3. Langchain Structured Output/Output Parsers/pydanticOutputParser.py
@@ -0,0 +1,38 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task = "text-generation"
+)
+
+
+model = ChatHuggingFace(llm=llm)
+
+
+# creating a pydantic object for output parsing and validation . This object is working as schema.
+class Person(BaseModel):
+
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18, description='Age of the person')   # Greater Than 18
+    city: str = Field(description='Name of the city the person belongs to')
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'sri lankan'})
+
+print(final_result)
